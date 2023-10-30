82bf17edc903741ec30cc8f745d613f81a0506fc
nguyethang083 <124845140+nguyethang083@users.noreply.github.com>
2023-10-31 03:12:04 +0700

    basic fxml

---
+++ src/main/java/DictionaryCommandLine/Dictionary.java
@@ -0,0 +1,7 @@
+package DictionaryCommandLine;
+
+import java.util.ArrayList;
+
+public class Dictionary {
+    public ArrayList<Word> list_word = new ArrayList<>();
+}
---
+++ src/main/java/DictionaryCommandLine/DictionaryApplication.java
@@ -0,0 +1,27 @@
+package DictionaryCommandLine;
+
+import com.example.dictionary.HelloApplication;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+import java.util.Objects;
+
+public class DictionaryApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
