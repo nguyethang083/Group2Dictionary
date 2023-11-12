54791b47fd9ff5b130ebc3510f44fb78794f0a63
nguyethang083 <124845140+nguyethang083@users.noreply.github.com>
2023-11-12 18:16:10 +0700

    API Translation added and controllers improved

--- src/main/java/Dictionary/DictionaryController/QuizController.java
+++ src/main/java/Dictionary/DictionaryController/QuizController.java
@@ -3,15 +3,9 @@ import Dictionary.Game.Quiz;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.paint.Color;
-
 
 import java.net.URL;
 import java.util.List;
