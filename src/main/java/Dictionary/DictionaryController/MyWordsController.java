e3a62f1c717224f3dce174edfa1f8a7d889ca355
nguyethang083 <124845140+nguyethang083@users.noreply.github.com>
2023-11-23 03:37:53 +0700

    saveWord on progress

---
+++ src/main/java/Dictionary/DictionaryController/MyWordsController.java
@@ -0,0 +1,11 @@
+package Dictionary.DictionaryController;
+
+import com.jfoenix.controls.JFXListView;
+import javafx.fxml.FXML;
+
+public class MyWordsController {
+
+    @FXML
+    private JFXListView<String> wordlist;
+
+}
